f8989fe4e6dc31e3a0fb6023625d969cd9aaadfd
Hoang-Minh <quataoxanh83@gmail.com>
2020-07-06 10:51:56 -0700

    setup using useState hook

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [advice, setAdvice] = useState("");
+
+  const fetchApi = async () => {
+    const id = Math.floor(Math.random() * 100) + 1;
+    const response = await axios.get(`https://api.adviceslip.com/advice/${id}`);
+    var data = JSON.parse(response.data + "}");
+    setAdvice(data.slip.advice);
+  };
+
+  useEffect(() => {
+    fetchApi();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="card">
+        <h1 className="heading">{advice}</h1>
+        <button className="button" onClick={fetchApi}>
+          <span>GIVE ME AN ADVICE !!!</span>
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
