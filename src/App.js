0f6b959a5e77be8fc667135ace43ff87859babe4
Hoang-Minh <quataoxanh83@gmail.com>
2020-07-06 13:57:33 -0700

    do not use App.css, instead use App.module.css for mobile responsive

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from "react";
 import axios from "axios";
-import "./App.css";
+import {
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+  Grow,
+} from "@material-ui/core";
+
+import styles from "./App.module.css";
 
 class App extends React.Component {
   state = { advice: "" };
@@ -26,14 +35,31 @@ class App extends React.Component {
 
   render() {
     const { advice } = this.state;
+
+    if (!advice) {
+      return null;
+    }
+
     return (
-      <div className="app">
-        <div className="card">
-          <h1 className="heading">{advice}</h1>
-          <button className="button" onClick={this.fetchApi}>
-            <span>GIVE ME AN ADVICE !!!</span>
-          </button>
-        </div>
+      <div className={styles.container}>
+        <Grow in={true}>
+          <Card>
+            <CardContent>
+              <Typography variant="h5" component="h5">
+                {advice}
+              </Typography>
+            </CardContent>
+            <CardActions className={styles.cardAction}>
+              <Button
+                variant="outlined"
+                color="primary"
+                onClick={this.fetchApi}
+              >
+                More
+              </Button>
+            </CardActions>
+          </Card>
+        </Grow>
       </div>
     );
   }
